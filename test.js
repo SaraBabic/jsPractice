9290e7e85377894d709785ec3f49396eec3ac081
SaraBabic <sarababic0101@gmail.com>
2023-12-17 20:13:27 +0100

    treci cas

---
+++ test.js
@@ -0,0 +1,36 @@
+// let x = 5;
+
+// function saberi(a,x) {  //funkcija
+//     console.log(x);
+
+//     function test() {
+//         console.log('testy');
+//         let y = 10;
+//     }
+
+
+//     return a + x;
+
+    
+// }
+
+
+function test(broj, funkcija) {
+    funkcija(broj);
+}
+
+function stampajBroj(broj) {
+    console.log(broj);
+}
+
+test(2,stampajBroj);
+
+
+// let saraMatematika = {  //objekat
+//     saberi: function(a,b) {  //metoda
+//         return a + b;
+//     },
+// };
+
+// console.log(saberi(2,2));
+// console.log(saraMatematika.saberi(2,3));
