ee5ae0351873b66c8d3c4ce4629b1822a7eaa61b
Sara <sara_babic@intention.de>
2024-11-21 21:50:30 +0100

    light and dark mode animation

--- sun/app.js
+++ sun/app.js
@@ -5,24 +5,60 @@ const sunPath =
   "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
 
 const darkMode = document.querySelector("#darkMode");
+const title = document.querySelector(".letters");
 let toggle = false;
-console.log("darkic ::", animate);
+var textWrapper = document.querySelector(".ml9 .letters");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
 
 darkMode.addEventListener("click", () => {
-  const timeline = animate.timeline({
+  const timeline = anime.timeline({
     duration: 750,
     easing: "easeOutExpo",
   });
   timeline
     .add({
       targets: ".sun",
-      d: [{ value: moonPath }],
+      d: [{ value: toggle ? sunPath : moonPath }],
     })
     .add(
       {
         targets: "#darkMode",
-        rotate: 320,
+        rotate: toggle ? 180 : 320,
       },
       "-= 350"
+    )
+    .add(
+      {
+        targets: "section",
+        backgroundColor: toggle ? "rgb(255,255,255)" : "rgb(22,22,22)",
+        color: toggle ? "rgb(22,22,22)" : "rgb(255,255,255)",
+      },
+      "-=700"
     );
+  if (!toggle) {
+    toggle = true;
+    title.textContent = "Dark mode";
+  } else {
+    title.textContent = "Light mode";
+    toggle = false;
+  }
 });
+
+anime
+  .timeline({ loop: false })
+  .add({
+    targets: ".ml9 .letter",
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i + 1),
+  })
+  .add({
+    targets: ".ml9",
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000,
+  });
