5d8c6fe5c7d587f54fcff454088017a359b3aca3
Sara <sara_babic@intention.de>
2024-11-24 20:41:19 +0100

    glass and fix animation

--- glass/app.js
+++ glass/app.js
@@ -1,5 +1,5 @@
-const moonPath =
-  "M16 27.5C16 42.6878 27.5 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 16 12.3122 16 27.5Z";
+const emptyGlass =
+  "M20.7515 31.9724L23.7515 4.97239M4.49999 32.0175L1.00001 5M21 31.5C21 33.433 17.1944 35 12.5 35C7.80558 35 4 33.433 4 31.5C4 29.567 7.80558 28 12.5 28C17.1944 28 21 29.567 21 31.5ZM24 4.5C24 6.433 18.8513 8 12.5 8C6.14873 8 1 6.433 1 4.5C1 2.567 6.14873 1 12.5 1C18.8513 1 24 2.567 24 4.5Z";
 
 const sunPath =
   "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
@@ -37,20 +37,7 @@ darkMode.addEventListener("click", () => {
         color: toggle ? "rgb(22,22,22)" : "rgb(255,255,255)",
       },
       "-=700"
-    )
-    .add({
-      targets: ".ml9 .letter",
-      scale: [0, 1],
-      duration: 1500,
-      elasticity: 600,
-      delay: (el, i) => 45 * (i + 1),
-    })
-    .add({
-      targets: ".ml9",
-      duration: 1000,
-      easing: "easeOutExpo",
-      delay: 1000,
-    });
+    );
   if (!toggle) {
     toggle = true;
     title.textContent = "Dark mode";
@@ -61,7 +48,7 @@ darkMode.addEventListener("click", () => {
 });
 
 anime
-  .timeline()
+  .timeline({ loop: false })
   .add({
     targets: ".ml9 .letter",
     scale: [0, 1],
